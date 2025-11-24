cb344730aa569be443ec642649cd9ebc9c78df98
Jack Carlton <j.carlton@uky.edu>
2025-11-24 01:09:41 -0500

    chore: group pipeline stages into subpackages

--- src/pioneerml/pipelines/stages/__init__.py
+++ src/pioneerml/pipelines/stages/__init__.py
@@ -4,13 +4,13 @@ Built-in pipeline stages for common operations.
 This package provides ready-to-use stages for typical ML workflows.
 """
 
-from pioneerml.pipelines.stages.load_data import LoadDataStage
-from pioneerml.pipelines.stages.save_data import SaveDataStage
-from pioneerml.pipelines.stages.train_model import TrainModelStage
-from pioneerml.pipelines.stages.inference import InferenceStage
-from pioneerml.pipelines.stages.lightning_train import LightningTrainStage
-from pioneerml.pipelines.stages.collect_preds import CollectPredsStage
-from pioneerml.pipelines.stages.evaluation import EvaluateStage
+from pioneerml.pipelines.stages.data.load_data import LoadDataStage
+from pioneerml.pipelines.stages.data.save_data import SaveDataStage
+from pioneerml.pipelines.stages.training.train_model import TrainModelStage
+from pioneerml.pipelines.stages.training.lightning_train import LightningTrainStage
+from pioneerml.pipelines.stages.inference.inference import InferenceStage
+from pioneerml.pipelines.stages.evaluation.collect_preds import CollectPredsStage
+from pioneerml.pipelines.stages.evaluation.evaluation import EvaluateStage
 
 __all__ = [
     "LoadDataStage",

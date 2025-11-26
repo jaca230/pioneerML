cd53adc1ed8af1baa16d8487db7d85ae07b46f6a
Jack Carlton <j.carlton@uky.edu>
2025-11-25 19:45:58 -0500

    added tutorial 5 for building lightning modules

--- src/pioneerml/zenml/pipelines/tutorial_examples/__init__.py
+++ src/pioneerml/zenml/pipelines/tutorial_examples/__init__.py
@@ -6,7 +6,13 @@ in tutorial notebooks to show different concepts.
 """
 
 from pioneerml.zenml.pipelines.tutorial_examples.quickstart_pipeline import quickstart_pipeline
+from pioneerml.zenml.pipelines.tutorial_examples.tabular_datamodule_pipeline import (
+    tabular_datamodule_pipeline,
+    TabularConfig,
+)
 
 __all__ = [
     "quickstart_pipeline",
+    "tabular_datamodule_pipeline",
+    "TabularConfig",
 ]

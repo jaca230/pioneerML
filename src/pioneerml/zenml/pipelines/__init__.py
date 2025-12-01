2ebf215fa5307b31928185f6fa3106c2c008dd88
Jack Carlton <j.carlton@uky.edu>
2025-12-01 06:59:08 -0500

    added an example notebook

--- src/pioneerml/zenml/pipelines/__init__.py
+++ src/pioneerml/zenml/pipelines/__init__.py
@@ -5,8 +5,16 @@ All example pipelines used in tutorials live under the ``tutorial_examples``
 module to keep educational flows organized in one place.
 """
 
+from pioneerml.zenml.pipelines.tutorial_examples.dummy_particle_grouping_pipeline import (
+    dummy_particle_grouping_pipeline,
+)
+from pioneerml.zenml.pipelines.tutorial_examples.dummy_particle_grouping_optuna_pipeline import (
+    dummy_particle_grouping_optuna_pipeline,
+)
 from pioneerml.zenml.pipelines.tutorial_examples.quickstart_pipeline import quickstart_pipeline
 
 __all__ = [
+    "dummy_particle_grouping_pipeline",
+    "dummy_particle_grouping_optuna_pipeline",
     "quickstart_pipeline",
 ]
--- src/pioneerml/zenml/pipelines/tutorial_examples/__init__.py
+++ src/pioneerml/zenml/pipelines/tutorial_examples/__init__.py
@@ -5,6 +5,12 @@ These pipelines demonstrate various ZenML patterns and can be used
 in tutorial notebooks to show different concepts.
 """
 
+from pioneerml.zenml.pipelines.tutorial_examples.dummy_particle_grouping_pipeline import (
+    dummy_particle_grouping_pipeline,
+)
+from pioneerml.zenml.pipelines.tutorial_examples.dummy_particle_grouping_optuna_pipeline import (
+    dummy_particle_grouping_optuna_pipeline,
+)
 from pioneerml.zenml.pipelines.tutorial_examples.quickstart_pipeline import quickstart_pipeline
 from pioneerml.zenml.pipelines.tutorial_examples.tabular_datamodule_pipeline import (
     tabular_datamodule_pipeline,
@@ -12,6 +18,8 @@ from pioneerml.zenml.pipelines.tutorial_examples.tabular_datamodule_pipeline imp
 )
 
 __all__ = [
+    "dummy_particle_grouping_pipeline",
+    "dummy_particle_grouping_optuna_pipeline",
     "quickstart_pipeline",
     "tabular_datamodule_pipeline",
     "TabularConfig",
